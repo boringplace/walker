d09c5a3b6d91a195c8c5c3973e70e5f49fe6cac8
Vlad Slepukhin <slp.vld@gmail.com>
2014-02-10 19:11:40 +0400

    'did filesystem walker and some rsync access to repos. now only RH-based'

---
+++ FSWalker.py
@@ -0,0 +1,27 @@
+import os
+class FSWalker:
+	global path
+	path = "./"
+
+	def __init__(self, tempdir):
+		self.path = path + tempdir
+		self.createTmpCatalog
+			
+	def goDown(self,cat):	
+		self.path += '/'+cat
+	
+	def goUp(self):
+		self.path = self.path.rsplit('/', 1)[0]
+
+	def createTmpCatalog(self):
+		if not os.path.exists(self.path):
+			os.mkdir(self.path)
+
+	def createCatalog(self,cat):
+		global path
+		self.goDown(cat)
+		if not os.path.exists(self.path):
+			os.mkdir(self.path)	
+
+	def getPath(self):
+		return self.path
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import subprocess
 import os
-# def createCatalog(cat):
 
+from FSWalker import FSWalker
+
+path = 'tmp'
 url = 'rsync://mirror.yandex.ru/'
+allowedRepos = ['fedora']
 
-path = './tmp'
+#creating worksapce
+fsw = FSWalker(path)
+fsw.createTmpCatalog()
 
-if not os.path.exists(path):
-	os.mkdir(path)
-
-
+#waiting for its own class
 cmdline = ["rsync", "--temp-dir="+path, url]
 
 proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE)
@@ -17,6 +19,21 @@ proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE)
 for line in proc.stdout:
 	try:
 		items = line.strip().split(None, 2)
-		print (url+items[0].decode("utf-8"))
+		item = items[0].decode("utf-8")
+		if item in allowedRepos:
+			fsw.createCatalog(item)
+			fsw.goUp()
 	except IndexError:
 		pass
+
+for item in allowedRepos:
+	fsw.goDown(item)
+	inner_url = url + item
+	cmd = ["rsync", "--temp-dir="+fsw.getPath(), inner_url]
+	aproc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+	for line in aproc.stdout:
+		items = line.strip().split(None, 2)
+		# print (items)
+		item = items[-1].decode("utf-8").rsplit(' ',1)[-1]
+		print (item)
+	print ("--------")
