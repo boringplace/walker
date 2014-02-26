2c9d85047921d08d7a21c24eabfdf1296e77d057
Vlad Slepukhin <slp.vld@gmail.com>
2014-02-26 20:04:52 +0400

    getting ISO files in obligatory way to provide greater compatibility

--- ParallelWorker.py
+++ ParallelWorker.py
@@ -26,26 +26,21 @@ def stepIn(url,urlForConfig,pxedir,d):
 	res = recursive_walk_directory(os.path.join(url,d))
 	templates = init_templates();	
 
-	#if no templates was found, walker will try to create ISO pxe.config
-	has_used_template = False
 	for elem in res:
 
 		for t in templates:
 			t.test_file(elem)
 			if t.test_complete():
 				t.build_directories(pxedir,urlForConfig,d,elem)
-				has_used_template = True
 				for t in templates:
 					t.reinit()
 				break
-
-	if not has_used_template:
-		isot = ISO_Template()
-		for elem in res:
-			isot.test_file(elem)
-			if isot.test_complete():
-				isot.build_directories(pxedir,urlForConfig,d,elem)
-				isot.reinit()
+	isot = ISO_Template()
+	for elem in res:
+		isot.test_file(elem)
+		if isot.test_complete():
+			isot.build_directories(pxedir,urlForConfig,d,elem)
+			isot.reinit()
 
 	print ('Checked: %s in %f'% (d,timeit.default_timer()-start))
 	generate_submenu_config('/'.join([pxedir,d]))
